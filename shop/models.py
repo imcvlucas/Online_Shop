de8e6969e0f539bebab96db420ce8f39f425b7dc
Lucas <imcvlucas@gmail.com>
2022-07-23 03:39:13 -0400

    added shoe products

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -18,12 +19,18 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    GENDER_CHOICES = (
+        ('Men', 'Men'),
+        ('Women', 'Women'),
+        ('Unisex', 'Unisex'),
+    )
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='draft')
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
