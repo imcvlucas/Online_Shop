bb7e9bd40c3607e1e2462e9cfc4b3c783b4229b1
Lucas <imcvlucas@gmail.com>
2022-07-25 04:34:14 -0400

    added sessions for cart

--- shop/admin.py
+++ shop/admin.py
@@ -11,7 +11,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created', 'updated']
+    list_display = ['name', 'slug',  'gender', 'price', 'available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'available']
+    list_editable = ['gender' , 'price', 'available']
     prepopulated_fields = {'slug': ('name',)}
